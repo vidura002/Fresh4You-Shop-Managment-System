ab2e8569797f1c89d2a8e2933688c64a7fa6dc98
Vidura Perera <137484172+vidura002@users.noreply.github.com>
2023-12-16 21:36:26 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    username : {
+        type : String,
+        required : ture,
+        unique : ture,
+    },
+    email : {
+        type : String,
+        required : ture,
+        unique : ture,
+    },
+    password : {
+        type : String,
+        required : ture,
+    },
+},{timestamps : true});
+
+const User = mongoose.model('User' , userSchema);
+
+export default User;
