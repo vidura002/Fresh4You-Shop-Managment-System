8d0a89ad0c8c64adcd9dc090518b5444151f3ff7
Vidura Perera <137484172+vidura002@users.noreply.github.com>
2023-12-28 15:41:26 +0530

    add create lisiting api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.router.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
+
 
 dotenv.config();
 
@@ -26,6 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <h1 className='hidden sm:inline text-slate-700 hover:underline text-xl'>Home</h1>
+    <h1 className='hidden sm:inline text-red-700 hover:underline text-3xl self-auto'>Home</h1>
   )
 }
