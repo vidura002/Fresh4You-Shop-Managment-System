7d1ad3bd23bb675b128ab3ca97ed9ef30b3d0b12
Vidura Perera <137484172+vidura002@users.noreply.github.com>
2024-01-23 12:07:43 +0530

    complete listing delete funtionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -145,6 +145,23 @@ export default function Profile() {
     }
   }
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE'
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }   
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -193,13 +210,13 @@ export default function Profile() {
             {userListings.map((listing) => (
             <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
               <Link to={`/listing/${listing._id}`}>
-              <img src={listing.imageUrls[0]} alt="listing image" className='h-16 w-16 object-contain '/>
+              <img src={listing.imageUrls[0]} alt="listing image" className='h-16 w-18 object-contain '/>
               </Link>
               <Link className='flex-1  font-semibold text-slate-700  hover:underline truncate' to={`/listing/${listing._id}`}>
                 <p className=' font-semibold text-slate-700  hover:underline truncate'>{listing.name}</p>
               </Link>
               <div className="flex flex-col">
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
