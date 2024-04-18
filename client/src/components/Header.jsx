f88206912472dcf1c926119f92d859362f7ac414
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-04-18 07:44:51 +0530

    add header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,16 @@ import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Logo from '../images/logo.jpg'
+import { LiaShoppingCartSolid } from "react-icons/lia";
+import { SlHandbag } from "react-icons/sl";
+
+
 
 export default function Header() {
-    const {currentUser} = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
+
+
     
     <header className='bg-green-400 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
@@ -16,11 +22,16 @@ export default function Header() {
             <span className='text-green-800'>4You</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-700'/>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-700" />
         </form>
-            <ul className='flex gap-6'>
+        <ul className='flex gap-6'>
                 <Link to={'/'}>
                 <li className='hidden sm:inline text-slate-700 hover:underline text-xl'>Home</li>
                 </Link>
@@ -34,11 +45,18 @@ export default function Header() {
                      <li className=' text-slate-700 hover:underline text-xl'>Sign In</li> 
                     )}
               </Link> 
+              <Link to={'/'}>
+                <li className='hidden sm:inline text-slate-700 font-medium hover:underline text-3xl'><LiaShoppingCartSolid /></li>
+                </Link>
+                <li className='hidden sm:inline text-slate-700 font-medium hover:underline text-xl'>|</li>
+                <Link to={'/'}>
+                <li className='hidden sm:inline text-slate-700 font-medium hover:underline text-2xl'><SlHandbag /></li>
+                </Link>
             </ul>
-        </div>
-        
+      </div>
     </header>
-   
-  )
-}
+  );
 
+            
+    
+}
