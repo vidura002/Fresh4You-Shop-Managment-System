e34b7b3120e72dc16ed323ad6fc3c372728e3566
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-04-22 04:41:45 +0530

    add loading in ti catalo page

--- client/src/pages/FruitCatelog.jsx
+++ client/src/pages/FruitCatelog.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import axios from "axios";
 import { Link, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-function FruitCatelog() {
+function FruitCatalog() {
   const location = useLocation();
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Fetch data from API
   //get all offers
@@ -19,8 +20,10 @@ function FruitCatelog() {
       );
       console.log("API :", response.data);
       setData(response.data.data);
+      setLoading(false);
     } catch (error) {
       console.error("Error :", error);
+      setLoading(false);
     }
   };
 
@@ -48,9 +51,9 @@ function FruitCatelog() {
       <Header />
       <div className="flex items-center justify-center text-2xl">
         <Link
-          to={"/FruitCatelog"}
+          to={"/FruitCatalog"}
           className={
-            location.pathname === "/FruitCatelog"
+            location.pathname === "/FruitCatalog"
               ? "text-black font-bold mt-5 hover:text-slate-500"
               : "text-gray-500 mt-5 hover:text-slate-500"
           }
@@ -76,53 +79,59 @@ function FruitCatelog() {
         <div></div>
         <div>
           <h2 className="text-lg font-semibold mb-4 ">Filters</h2>
-          <label htmlFor="search ">Search:</label>
+          <label htmlFor="search">Search:</label>
           <input
             type="text"
             id="search"
             onChange={(e) => handleFilter(e.target.value)}
-            className="border border-gray-300 rounded-md px-2 py-1 mb-4 block w-96 "
+            className="border border-gray-300 rounded-md px-2 py-1 mb-4 block w-96"
           />
         </div>
         <div></div>
       </div>
       <br />
-      <ul className="grid gap-5 md:grid-cols-3 lg:grid-cols-5 ml-10 mr-10 ">
-        {filteredData.map((item, index) => (
-          <li
-            key={index}
-            className="border border-gray-200 rounded-md overflow-hidden animate-fade-in shadow-xl"
-          >
-            <div className="flex items-center justify-center ">
-              <img src={item.image} alt={item.FruitName} className="" />
-            </div>
-            <div className="p-4">
-              <h2 className="text-lg font-semibold">{item.FruitName}</h2>
-              <p className="text-red-600 text-2xl">
-                Rs.{item.price}.00{" "}
-                <span className="text-black text-lg">[Per 100g]</span>
-              </p>
-              <br />
-              <button
-                className={`w-full bg-green-800 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:bg-indigo-600 animate-fade-in ${
-                  item.FruitQuantity === 0
-                    ? "bg-red-500 cursor-not-allowed hover:bg-red-500 "
-                    : ""
-                }`}
-                disabled={item.FruitQuantity === 0}
+      
+      <Suspense fallback={<div className="text-center text-6xl">Loading...</div>}>
+        <ul className="grid gap-5 md:grid-cols-3 lg:grid-cols-5 ml-10 mr-10">
+          {loading ? (
+            <div className="text-center my-4">Loading...</div>
+          ) : (
+            filteredData.map((item, index) => (
+              <li
+                key={index}
+                className="border border-gray-200 rounded-md overflow-hidden animate-fade-in shadow-xl"
               >
-                {item.FruitQuantity === 0 ? "Sold Out" : "Add to Cart"}
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <br />
-
+                <div className="flex items-center justify-center">
+                  <img src={item.image} alt={item.FruitName} className="" />
+                </div>
+                <div className="p-4">
+                  <h2 className="text-lg font-semibold">{item.FruitName}</h2>
+                  <p className="text-red-600 text-2xl">
+                    Rs.{item.price}.00{" "}
+                    <span className="text-black text-lg">[Per 100g]</span>
+                  </p>
+                  <br />
+                  <button
+                    className={`w-full bg-green-800 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:bg-indigo-600 animate-fade-in ${
+                      item.FruitQuantity === 0
+                        ? "bg-red-500 cursor-not-allowed hover:bg-red-500 "
+                        : ""
+                    }`}
+                    disabled={item.FruitQuantity === 0}
+                  >
+                    {item.FruitQuantity === 0 ? "Sold Out" : "Add to Cart"}
+                  </button>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </Suspense>
+    
       <br />
       <Footer />
     </div>
   );
 }
 
-export default FruitCatelog;
+export default FruitCatalog;
