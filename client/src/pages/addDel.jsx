7914af7b1df686f619040c4e3f2515404dd8eae4
Jeewapadma <127758408+IT22274120@users.noreply.github.com>
2024-04-29 11:44:57 +0530

    Update addDel.jsx

--- client/src/pages/addDel.jsx
+++ client/src/pages/addDel.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function addDel() {
   return (
-    <div><h1>udith
+    <div><h1>
+        udith
         </h1>
     </div>
   )
 }
 
-export default addDel
+export default addDel;
