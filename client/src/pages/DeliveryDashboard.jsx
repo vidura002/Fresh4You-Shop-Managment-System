f3c1b92996dad7056dd3943c5170277f36e555f3
Jeewapadma <127758408+IT22274120@users.noreply.github.com>
2024-05-05 14:44:31 +0530

    deliverer dashboard adjustment

--- client/src/pages/DeliveryDashboard.jsx
+++ client/src/pages/DeliveryDashboard.jsx
@@ -9,7 +9,7 @@ function DeliveryDashboard() {
 
 <div>
     <Header />
-    <div className="min-h-svh bg-[url('images/delivery.jpg')] bg-left bg-cover bg-right screens-md">
+    <div className="min-h-svh bg-[url('images/delivery.jpg')] bg-left bg-cover bg-left screens-md">
         <div className='p-32' >
             <span className='text-6xl font-semibold'>Welcome to DashBoard!</span>
             <h1 className='grid grid-cols-2 text-3xl text-justify antialiased'><br/><br/> The Deliverer Dashboard is now available! This is your main hub for effectively tracking and managing your delivery tasks. This is where you can monitor and approve incoming delivery requests, follow the status of assigned deliveries in real time, and get in touch with clients or customer service representatives as needed. Remain informed, remain organised, and present with assurance!"<br/></h1><br/>
